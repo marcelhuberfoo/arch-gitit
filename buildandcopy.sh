7f2a4703742143d92b27cbe6c07841bf23e486d7
Marcel Huber <marcelhuberfoo@gmail.com>
2015-01-07 14:00:31 +0100

    update of aur specific commands

--- buildandcopy.sh
+++ buildandcopy.sh
@@ -2,12 +2,15 @@
 
 thedirs="."
 repodir=/home/own-repo
+category=network
 
 thingsToDo=(
   'for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --noprogressbar --nodeps --nobuild ); done'
   'git diff --quiet --no-ext-diff --exit-code && echo clean || ( git diff -b -w && git add -p && git commit ) || true'
   'for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --noprogressbar --nodeps ); done'
-  'test -n "$repodir" -a -d "$repodir" && for n in $thedirs; do for pkgdir in $(find $n -name "*.tar.xz" -printf "%h\n" | sort | uniq); do cpfn=$(ls -tc1 $pkgdir/*.tar.xz | head -1); echo "copy [$cpfn]? (y*|n)"; read v; if [ ! "$v" = "n" -a ! "$v" = "N" ]; then sudo sh -c "cp --interactive --preserve --update $cpfn $repodir && ( repo-add --new --quiet ${repodir}/ownrepo.db.tar.gz $repodir/$(basename $cpfn); repo-add --files --new --quiet ${repodir}/ownrepo.files.tar.gz $repodir/$(basename $cpfn))"; fi; done; done'
+  'mkaurball -ef'
+  'upfile=$(ls -tc1 *.src.tar.gz | head -1); test -n "$upfile" && ( echo uploading $upfile; burp --user=marcelhuber --category=$category $upfile)'
+  'test -n "$repodir" -a -d "$repodir" && for n in $thedirs; do for pkgdir in $(find $n -name "*.tar.xz" -printf "%h\n" | sort | uniq); do cpfn=$(ls -tc1 $pkgdir/*.tar.xz | head -1); echo "copy [$cpfn]? (y*|n)"; read v; if [ ! "$v" = "n" -a ! "$v" = "N" ]; then sh -c "cp --interactive --preserve --update $cpfn $repodir && ( repo-add --new --quiet ${repodir}/ownrepo.db.tar.gz $repodir/$(basename $cpfn); repo-add --files --new --quiet ${repodir}/ownrepo.files.tar.gz $repodir/$(basename $cpfn))"; fi; done; done'
 )
 
 set -e
